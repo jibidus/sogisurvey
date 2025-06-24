6ca2564524966eff4a5374b3151581c176688427
Jibidus <jibidus@gmail.com>
2025-06-24 09:17:57 +0200

    :tada: sort criteria

--- src/main/kotlin/com/sogilis/survey/Criterions.kt
+++ src/main/kotlin/com/sogilis/survey/Criterions.kt
@@ -11,36 +11,53 @@ data class CriterionInput(
 
 val CRITERIA = listOf(
     CriterionInput(
-        id = "env-confort", title = "Confort de l'environnement de travail chez le client",
-        commentPlaceholder = "Plus de précision…"
-    ),
-    CriterionInput(
-        id = "dev-constraints", title = "Faibles contraintes sur l'environnement de dev",
-        commentPlaceholder = "Ex: choix de l'OS, possibilité d'utiliser les outils Sogilis…"
-    ),
-    CriterionInput(
         id = "localisation", title = "Localisation du client",
         commentPlaceholder = "Ex: proche gare, à l'ouest de Grenoble…"
-    ),
-    CriterionInput(
-        id = "gestion-projet", title = "Type de gestion de projet",
-        commentPlaceholder = "Ex: Itérations, Scrum, XP…"
-    ),
-    CriterionInput(
-        id = "autonomie", title = "Autonomie",
-        commentPlaceholder = "Plus de précision…"
     ),
     CriterionInput(
         id = "duree-mission", title = "Durée de mission",
         commentPlaceholder = "Longue ou courte, combien de mois/années …"
     ),
     CriterionInput(
+        id = "type-contrat", title = "Type de contrat",
+        commentPlaceholder = "Ex: forfait, assistance Technique…"
+    ),
+    CriterionInput(
+        id = "amelioration-continue", title = "Le client souhaite s'améliorer et est à l'écoute de propositions",
+        commentPlaceholder = "Plus de précision…"
+    ),
+    CriterionInput(
         id = "domaine-metier", title = "Domaine métier",
         commentPlaceholder = "Ex: médical, éthique, IA…"
     ),
     CriterionInput(
-        id = "type-contrat", title = "Type de contrat",
-        commentPlaceholder = "Ex: forfait, assistance Technique…"
+        id = "autonomie", title = "Autonomie pour adresser les besoins exprimés",
+        commentPlaceholder = "Plus de précision…"
+    ),
+    CriterionInput(
+        id = "progression", title = "La mission me fait progresser rapidement",
+        commentPlaceholder = "Ex: techniquement, en pratiques, en compétences humaines…"
+    ),
+    CriterionInput(
+        id = "ambiance", title = "Ambiance avec le client",
+        commentPlaceholder = "Plus de précision…"
+    ),
+    CriterionInput(
+        id = "team-sogilis", title = "Pouvoir travailler en équipe Sogilis",
+        commentPlaceholder = "Plus de précision…"
+    ),
+    // dev
+    CriterionInput(
+        id = "gestion-projet", title = "Type de gestion de projet",
+        commentPlaceholder = "Ex: Itérations, Scrum, XP…"
+    ),
+    CriterionInput(
+        id = "env-confort", title = "Confort de l'environnement de travail chez le client",
+        commentPlaceholder = "Bureau attitré, salle de réunion disponible, locaux bien équipés…"
+    ),
+    CriterionInput(
+        id = "dev-constraints", title = "Contraintes sur l'environnement de dev",
+        commentPlaceholder = "Ex: choix de l'OS, accès aux outils Sogilis, utilisation de Teams…"
     ),
     CriterionInput(
         id = "base-code", title = "Base de code existante",
@@ -51,27 +68,7 @@ val CRITERIA = listOf(
         commentPlaceholder = "Ex: front, web, mobile, embarqué, algo……"
     ),
     CriterionInput(
-        id = "language", title = "Language",
-        commentPlaceholder = "Ex: language de haut niveau, language moderne…"
-    ),
-    CriterionInput(
-        id = "technos", title = "Technos",
-        commentPlaceholder = "Ex: React, Rails, Quarkus…"
-    ),
-    CriterionInput(
-        id = "progression", title = "La mission me fait progresser rapidement",
-        commentPlaceholder = "Ex: techniquement, en pratiques, en compétences humaines…"
-    ),
-    CriterionInput(
-        id = "ambiance", title = "Ambiance humaine",
-        commentPlaceholder = "Plus de précision…"
-    ),
-    CriterionInput(
-        id = "team-sogilis", title = "Pouvoir travailler en équipe Sogilis",
-        commentPlaceholder = "Plus de précision…"
-    ),
-    CriterionInput(
-        id = "amelioration-continue", title = "Le client souhaite s'améliorer en continue",
-        commentPlaceholder = "Plus de précision…"
+        id = "technos-langages", title = "Technos, langages",
+        commentPlaceholder = "Ex: C++, Ruby, React, Rails, Quarkus…"
     ),
 )
